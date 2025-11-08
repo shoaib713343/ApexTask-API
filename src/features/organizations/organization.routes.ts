00de6a069b1775cb3313d1af83bb5847da76e644
Shoaib <shoaib713343@gmail.com>
2025-11-08 13:47:04 +0530

    fixing some runtime errors

--- src/features/organizations/organization.routes.ts
+++ src/features/organizations/organization.routes.ts
@@ -10,7 +10,7 @@ import boardRouter from "../boards/board.route";
 const router = Router();
 
 router.get("/:orgId", protect, asyncHandler(getOrganizationController));
-router.delete(":/orgId", protect, asyncHandler(deleteOrgController));
+router.delete("/:orgId", protect, asyncHandler(deleteOrgController));
 router.delete("/:orgId/:userId", protect, asyncHandler(removeUserController));
 router.patch("/:orgId/members/:userId", protect, validate(updateRoleSchema), asyncHandler(updateRoleController));
 router.post("/:orgId/invite", protect, validate(inviteSchema), asyncHandler(inviteController));
